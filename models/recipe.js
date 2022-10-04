fb958360100a8c1557852af5e48570129eebdd70
harrisimon <harrisimon@gmail.com>
2022-10-03 20:32:55 -0400

    fully crud-ing

--- models/recipe.js
+++ models/recipe.js
@@ -3,18 +3,16 @@ const mongoose = require("mongoose")
 const { Schema, model } = mongoose
 
 //recipe schema
+const ingredientSchema = new Schema({
+    name: String,
+    amount: Number
+})
 const recipeSchema = new Schema({
     name: String,
-    ingredientOne: {
-        name: String,
-        amount: Number
-    },
-    ingredientTwo: {
-        name: String,
-        amount: Number
-    },
+    ingredients: [ingredientSchema],
     isNaturallyGF: Boolean
 })
+
 
 const Recipe = model('Recipe', recipeSchema)
 
